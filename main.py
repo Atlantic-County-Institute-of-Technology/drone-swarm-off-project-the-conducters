7d5c18408568fdd56c7b51bd1fef0d7712a2c5e1
Adir <105810536+AdirAdam@users.noreply.github.com>
2024-01-16 10:41:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,21 +8,36 @@ drone = Drone()
 drone.pair()
 # drone.takeoff()
 
-def Spiral(seconds):
+def Spiral(seconds, upDown):
+    global roll, throttle
+    if upDown == "up":
+        throttle = 50
+        roll = 20
+    elif upDown == "down":
+        throttle = -50
+        roll = -20
     for x in range(3):
-        drone.go(20, 0, 50, 15, seconds)
-    drone.set_yaw(100)
+        drone.go(roll, 0, throttle, 15, seconds)
+    drone.set_yaw(-100)
     drone.move(1)
     drone.land()
 
 def Sway(amountSway):
     for x in range(amountSway):
         drone.set_roll(50)
-        drone.move(1.5)
-        time.sleep(2)
+        drone.move(.5)
+        time.sleep(1)
         drone.set_roll(-50)
-        drone.move(3)
-        time.sleep(2)
+        drone.move(1.5)
+        time.sleep(1.5)
+
+def UpDown(upDown):
+    for x in range(upDown):
+        drone.go(0, 0, 0, 90, upDown)
+        drone.hover(0.5)
+        drone.go(0,0,0,-90, upDown)
+        drone.hover(0.5)
+
 
 
 
@@ -30,15 +45,22 @@ def Sway(amountSway):
     # code that section, light code basic movment
 
 drone.takeoff()
-drone.hover(2)
-Sway(2)
+drone.hover(1)
+# Sway(2)
+
+Spiral(2, "up")
+
 
 
 
 # 2:17-2:18 is where it starts to move, left and right
 
-drone.hover(1)
-# Spiral(2)
+drone.hover(2)
+
+Spiral(2, "down")
+# UpDown(4)
+
+
 
 
 # Music 2:23 - 3:30, build up
